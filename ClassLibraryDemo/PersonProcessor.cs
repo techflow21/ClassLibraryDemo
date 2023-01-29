ccf9bd1e92e5303bb2e6232a507552aa77ab2d0e
Bello Soliu <bellosoliu12@gmail.com>
2023-01-29 17:03:43 +0100

    Add project files.

---
+++ ClassLibraryDemo/PersonProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibraryDemo;
+
+public class PersonProcessor
+{
+    public static string GetPersonDetails(string firstName, string lastName)
+    {
+        string output = $"{firstName} {lastName}";
+
+        return output;
+    }
+}
---
+++ DemoLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibraryDemo;
+
+namespace DemoLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name:");
+            string? firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            string? lastName = Console.ReadLine();
+
+            string response = PersonProcessor.GetPersonDetails(firstName, lastName);
+            Console.WriteLine($"The Person full name is : {response}");
+        }
+    }
+}
